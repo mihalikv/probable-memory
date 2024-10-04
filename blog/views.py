491c27035e01950fed2c8114f5dd1c3a9d837018
mihalik.vilko <mihalik.vilko@gmail.com>
2024-10-04 12:47:39 +0200

    triv: removed import added comment

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,17 @@ from typing import Any
 
 from django.views.generic import TemplateView
 
-from blog.models import Book
-
-
 class BookListView(TemplateView):
     template_name = "list.html"
 
     def get_context_data(self, *args, **kwargs: Any) -> dict[str, Any]:
         context_data = super().get_context_data(**kwargs)
         context_data["title"] = 'Test 1'
+        # Problems:
+        # 1. show title first 10 books on URL http://127.0.0.1:8000/test-1/ 
+        # 2. restrict books to just books with is_active=True or is_super_active=True
+        # 3. show total number of books under list of books
+        # 4. show author name next to book title 
         return context_data
 
 class AuthorListView(TemplateView):
@@ -19,4 +21,7 @@ class AuthorListView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context_data = super().get_context_data(**kwargs)
         context_data["title"] = 'Test 2'
+        # Problems:
+        # 1. show list of authors on URL http://127.0.0.1:8000/test-2/ 
+        # 2. show number of books foe each author
         return context_data
